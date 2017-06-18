1e0d3d139bda88b2c7be368da449a379ede8c38d
Francesco Cannizzaro <francisthemoderator@gmail.com>
2017-06-18 16:17:20 +0200

    fix crash

--- npm-install.py
+++ npm-install.py
@@ -1,12 +1,69 @@
 import sublime_plugin
 import webbrowser as browser
 from sublime import Region
+import subprocess
 import threading
 import sublime
 import re
-from . import npm
 
+module_regex = r'.*require\(["\']([^.].*?)["\']\).*'
 NPMJS = 'https://www.npmjs.com/package/'
+
+def icons(view):
+  regions = view.find_all(module_regex)
+  modules = []
+
+  for region in regions:
+    text = view.substr(region)
+    m = re.search(module_regex, text)
+    a,b = m.span(1)
+    if m.group(1)[0] != '-':
+      modules.append(Region(a + region.begin(),b + region.begin()))
+
+  flags = sublime.HIDE_ON_MINIMAP | sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE
+  view.add_regions('require', regions, 'npm', 'Packages/npm-install/icon.png', flags)
+
+  flags |= sublime.DRAW_SOLID_UNDERLINE
+  view.add_regions('modules', modules, 'modules', flags=flags)
+
+  return regions
+
+def is_valid(view):
+  return view.file_name().endswith('.js')
+
+def modules(root):
+  modules = []
+  p = subprocess.Popen(['npm', 'ls', '-parseable', '-depth=0'], shell=True, cwd=root, stdout=subprocess.PIPE)
+  next(p.stdout)
+  for line in p.stdout:
+    m = re.match(r'.*(?:\\\\|/)(.*)\\.*', str(line))
+    if m:
+      modules.append(m.group(1))
+  return modules
+
+def install(module, view, root):
+  Exec(module, view.window(), root, 'install').start()
+
+def uninstall(module, view, root, region):
+  Exec(module, view.window(), root, 'uninstall', region, view).start()
+
+class Exec(threading.Thread):
+
+    def __init__(self, module, window, root, action, region=None, view=None):
+        self.module = module
+        self.window = window
+        self.root = root
+        self.action = action
+        self.region = region
+        self.view = view
+        threading.Thread.__init__(self)
+
+    def run(self):
+      self.window.status_message('%sing %s' % (self.action, self.module))
+      subprocess.Popen(['npm', self.action, self.module, '-s'], shell=True, cwd=self.root)
+      print('npm', self.action, self.module)
+      if self.view:
+        self.view.run_command("npm_clear", { 'a': self.region.a, 'b': self.region.b })
 
 def update_settings():
   install_on_save = settings.get('install_on_save')
@@ -22,13 +79,13 @@ class NpmDocCommand(sublime_plugin.TextCommand):
     sel = self.view.sel()[0]
     line = self.view.full_line(sel)
     req = self.view.substr(line)
-    match = re.match(npm.module_regex, req)
+    match = re.match(module_regex, req)
     if match: browser.open(NPMJS + match.group(1))
  
 class NpmInstallCommand(sublime_plugin.TextCommand):
 
     def is_visible(self):
-        return npm.is_valid(self.view)
+        return is_valid(self.view)
 
     def run(self, edit):
       Command(self.view, edit).start()
@@ -47,11 +104,11 @@ class Command(threading.Thread):
 
     def run(self):
 
-      if not npm.is_valid(self.view):
+      if not is_valid(self.view):
         return
 
       # mark line as npm module
-      regions = npm.icons(self.view)
+      regions = icons(self.view)
 
       if not len(regions):
         return
@@ -59,27 +116,27 @@ class Command(threading.Thread):
       # list npm modules
       project = re.match(r'(.*)[\/\\].*', self.view.file_name())
       cwd = project.group(1)
-      modules = npm.modules(cwd)
+      modules = modules(cwd)
       
       # find modules and install/uninstall
       for region in regions:
         
-        match = re.match(npm.module_regex, self.view.substr(region))
+        match = re.match(module_regex, self.view.substr(region))
         module = match.group(1)
         
         if module[0] == '-':
-           npm.uninstall(module[1:], self.view, cwd, region)           
+           uninstall(module[1:], self.view, cwd, region)           
 
         elif module not in modules:
-          npm.install(module, self.view, cwd)
+          install(module, self.view, cwd)
 
 class EventEditor(sublime_plugin.EventListener):
 
     def on_modified_async(self, view):
-        npm.icons(view)
+        icons(view)
         
     def on_activated_async(self, view):
-        npm.icons(view)
+        icons(view)
 
     def on_post_save_async(self, view):
         if install_on_save:
